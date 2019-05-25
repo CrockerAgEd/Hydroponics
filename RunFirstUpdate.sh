3cb0318ebf2018201478f7683ff6e7950a0238f3
CrockerFFA <45186785+CrockerFFA@users.noreply.github.com>
2019-05-24 20:51:13 -0500

    Create RunFirstUpdate.sh

---
+++ RunFirstUpdate.sh
@@ -0,0 +1,19 @@
+cd ..
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential python-dev
+#needs install
+git clone https://github.com/adafruit/io-client-python
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+cd Adafruit_Python_DHT
+sudo apt-get install build-essential python-dev
+sudo python setup.py install
+sudo python3 setup.py install
+pip3 install adafruit-io
+
+
+#test
+cd ..
+cd Adafruit_Python_DHT
+cd examples
+python AdafruitDHT.py 11 20
